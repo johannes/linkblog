5a25bfc385941fd5a2500001cf45941ee6334d92
Johannes Schlueter <johannes@igloo.chinstrap.eu>
2008-02-07 00:19:25 +0100

    A bit more OOP for the JavaScript code

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,48 +4,58 @@ For using this add a bookmark similar to:
 javascript:(function(){jl_url='http://schlueters.de/links/';jl_token='TOKEN';var d=document;if(!d.getElementById("jl_overlay")){var s=d.createElement('script');s.src=jl_url+"bookmarklet.js";s.id="jl_overlay";d.body.appendChild(s);}})();
 
 You should at least change jl_url and jl_token there, set jl_token to a 
-ulr-encoded name=value pair which you check in auth.php
+url-encoded name=value pair which you check in auth.php
 
 */
 
-var jl_overlay;
-
-function jl_submit_url(url) {
-    window.jl_iframe.location.href = jl_url+'addlink.php?url='+escape(url)+'&'+jl_token;
+function JohannesLinks(url, token)
+{
+    this.URL   = url;
+    this.Token = token;
+    this.createOverlay();
+    this.submitCurrent();
 }
 
-function jl_submit_current() {
-    jl_submit_url(window.location.href);
+JohannesLinks.prototype.URL     = "";
+JohannesLinks.prototype.Token   = "";
+JohannesLinks.prototype.Overlay = null;
+
+JohannesLinks.prototype.submitURL = function(url) {
+    window.jl_iframe.location.href = this.URL+'addlink.php?url='+escape(url)+'&'+this.Token;
 }
 
-function jl_createOverlay() {
+JohannesLinks.prototype.submitCurrent = function() {
+    this.submitURL(window.location.href);
+}
+
+JohannesLinks.prototype.createOverlay = function() {
     var d = document;
-    jl_overlay = d.createElement('div');
-    jl_overlay.setAttribute('style', 'position:absolute; top 10px; left: 10px; background-color: #fff;border: 2px solid black;');
+    this.Overlay = d.createElement('div');
+    this.Overlay.setAttribute('style', 'position:absolute; top 10px; left: 10px; background-color: #fff;border: 2px solid black;');
 
-    jl_overlay.innerHTML = ''
-        +'<form action="" name="jl_tag_frm" onsubmit="return jl_submittags()">'
+    this.Overlay.innerHTML = ''
+        +'<form action="" name="jl_tag_frm" onsubmit="return jl.submitTags()">'
         +'<input type="text" name="tags"> <input type="submit">'
         +'</form>'
         +'<iframe style="display:none;" name="jl_iframe"></iframe>'
         ;
 
-    d.getElementsByTagName('body').item(0).appendChild(jl_overlay);
+    d.getElementsByTagName('body').item(0).appendChild(this.Overlay);
 }
 
-function jl_submittags() {
-    window.jl_iframe.location.href = jl_url
+JohannesLinks.prototype.submitTags = function() {
+    window.jl_iframe.location.href = this.URL
                                    + 'settag.php?url='
                                    + escape(window.location.href)
                                    + '&tags='
                                    + escape(document.jl_tag_frm.tags.value)
                                    + '&'
-                                   + jl_token;
+                                   + this.Token;
 
-    jl_overlay.style.display = "none";
+    this.Overlay.style.display = "none";
 
     return false;
 }
 
-jl_createOverlay();
-jl_submit_current();
+jl = new JohannesLinks(jl_url, jl_token); 
+
